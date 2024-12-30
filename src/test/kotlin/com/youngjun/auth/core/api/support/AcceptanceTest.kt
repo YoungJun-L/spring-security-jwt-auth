92daf6a4f8365362ae999982a4751e1aae6d235a
YoungJun-L <dudwns328@naver.com>
2024-12-30 12:18:06 +0900

    document 확장 함수 이동

--- src/test/kotlin/com/youngjun/auth/core/api/support/AcceptanceTest.kt
+++ src/test/kotlin/com/youngjun/auth/core/api/support/AcceptanceTest.kt
@@ -2,14 +2,8 @@ package com.youngjun.auth.core.api.support
 
 import com.youngjun.auth.AuthApplication
 import io.kotest.core.annotation.Tags
-import io.restassured.specification.RequestSpecification
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor
-import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor
-import org.springframework.restdocs.operation.preprocess.Preprocessors
-import org.springframework.restdocs.restassured.RestAssuredRestDocumentation
-import org.springframework.restdocs.snippet.Snippet
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestConstructor
 import org.springframework.test.context.TestExecutionListeners
@@ -30,23 +24,3 @@ import org.springframework.test.context.TestExecutionListeners
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class AcceptanceTest
-
-fun RequestSpecification.document(
-    identifier: String?,
-    vararg snippets: Snippet?,
-): RequestSpecification =
-    filter(
-        RestAssuredRestDocumentation.document(identifier, requestPreprocessor(), responsePreprocessor(), *snippets),
-    )
-
-private fun requestPreprocessor(): OperationRequestPreprocessor =
-    Preprocessors.preprocessRequest(
-        Preprocessors
-            .modifyUris()
-            .scheme("http")
-            .host("dev.youngjun.com")
-            .removePort(),
-        Preprocessors.prettyPrint(),
-    )
-
-private fun responsePreprocessor(): OperationResponsePreprocessor = Preprocessors.preprocessResponse(Preprocessors.prettyPrint())
