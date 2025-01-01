b03f09314152b870d5aec0ef21cee1e8cd617827
YoungJun-L <dudwns328@naver.com>
2025-01-01 17:56:00 +0900

    Jwt 인증 문서 추가

--- src/test/kotlin/com/youngjun/auth/core/api/security/JwtAuthenticationTest.kt
+++ src/test/kotlin/com/youngjun/auth/core/api/security/JwtAuthenticationTest.kt
@@ -2,6 +2,7 @@ package com.youngjun.auth.core.api.security
 
 import com.youngjun.auth.core.api.support.AcceptanceTest
 import com.youngjun.auth.core.api.support.context
+import com.youngjun.auth.core.api.support.document
 import com.youngjun.auth.core.api.support.response.AuthResponse
 import com.youngjun.auth.core.domain.token.TokenParser
 import io.kotest.core.spec.IsolationMode
@@ -13,6 +14,8 @@ import io.restassured.RestAssured.given
 import io.restassured.http.ContentType
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.restdocs.headers.HeaderDocumentation.headerWithName
+import org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -30,8 +33,21 @@ class JwtAuthenticationTest(
                     val username = "username123"
                     every { tokenParser.parseSubject(accessToken) } returns username
 
-                    val results = authenticate(accessToken)
-                    results.statusCode() shouldBe HttpStatus.OK.value()
+                    given()
+                        .log()
+                        .all()
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer $accessToken")
+                        .contentType(ContentType.JSON)
+                        .document(
+                            "authenticate",
+                            requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION).description("JWT access token").optional(),
+                            ),
+                        ).get("/test")
+                        .then()
+                        .log()
+                        .all()
+                        .statusCode(HttpStatus.OK.value())
                 }
 
                 test("유효하지 않은 토큰인 경우") {
