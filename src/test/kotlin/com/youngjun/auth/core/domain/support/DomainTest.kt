efc4e5990b8c92fd80dbfc3bdbe4ef05d2069f3c
YoungJun-L <dudwns328@naver.com>
2025-01-02 22:29:07 +0900

    DomainTest 변경

--- src/test/kotlin/com/youngjun/auth/core/domain/support/DomainTest.kt
+++ src/test/kotlin/com/youngjun/auth/core/domain/support/DomainTest.kt
@@ -1,17 +1,10 @@
 package com.youngjun.auth.core.domain.support
 
+import com.youngjun.auth.core.api.support.ApplicationTest
 import io.kotest.core.annotation.Tags
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.TestConstructor
 
 @Tags("domain")
-@ActiveProfiles("test")
-@SpringBootTest
-@EnableAutoConfiguration(exclude = [SecurityAutoConfiguration::class])
-@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@ApplicationTest
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class DomainTest
