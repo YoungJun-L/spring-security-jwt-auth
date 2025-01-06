f65d801941ecefdafb21e8efc20db57211805b13
YoungJun-L <dudwns328@naver.com>
2025-01-07 01:33:33 +0900

    token generate 테스트 추가

--- src/test/kotlin/com/youngjun/auth/core/domain/token/TokenPairGeneratorTest.kt
+++ src/test/kotlin/com/youngjun/auth/core/domain/token/TokenPairGeneratorTest.kt
@@ -2,11 +2,14 @@ package com.youngjun.auth.core.domain.token
 
 import com.youngjun.auth.core.domain.auth.AuthBuilder
 import com.youngjun.auth.core.domain.support.DomainTest
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
 import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.extensions.spring.SpringExtension
 import io.kotest.extensions.time.withConstantNow
 import io.kotest.matchers.shouldBe
+import org.springframework.beans.factory.annotation.Value
 import java.time.LocalDateTime.now
 import java.time.ZoneOffset
 import kotlin.time.Duration.Companion.days
@@ -15,12 +18,23 @@ import kotlin.time.Duration.Companion.minutes
 @DomainTest
 class TokenPairGeneratorTest(
     private val tokenPairGenerator: TokenPairGenerator,
+    @Value("\${spring.security.jwt.secret-key}") private val secretKey: String,
 ) : FunSpec(
         {
             extensions(SpringExtension)
             isolationMode = IsolationMode.InstancePerLeaf
 
             context("발급") {
+                test("성공") {
+                    val auth = AuthBuilder().build()
+
+                    val actual = tokenPairGenerator.issue(auth)
+
+                    val parser = Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secretKey.toByteArray())).build()
+                    parser.parseSignedClaims(actual.accessToken).payload.subject shouldBe auth.username
+                    parser.parseSignedClaims(actual.refreshToken).payload.subject shouldBe auth.username
+                }
+
                 test("access token 은 30분간 유효하다.") {
                     val now = now()
                     withConstantNow(now) {
