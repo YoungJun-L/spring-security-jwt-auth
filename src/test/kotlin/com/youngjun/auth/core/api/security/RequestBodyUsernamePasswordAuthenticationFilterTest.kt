e66de2d54f0a6f0eb8f4a001c98edef783be9705
YoungJun-L <dudwns328@naver.com>
2025-01-13 10:15:47 +0900

    RequestBodyUsernamePasswordAuthenticationFilter 테스트 컨테이너 없는 방식으로 변경

--- src/test/kotlin/com/youngjun/auth/core/api/security/RequestBodyUsernamePasswordAuthenticationFilterTest.kt
+++ src/test/kotlin/com/youngjun/auth/core/api/security/RequestBodyUsernamePasswordAuthenticationFilterTest.kt
@@ -1,68 +1,68 @@
 package com.youngjun.auth.core.api.security
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.youngjun.auth.core.api.application.TokenService
-import com.youngjun.auth.core.api.support.SecurityTest
-import com.youngjun.auth.core.domain.user.UserBuilder
-import com.youngjun.auth.core.domain.user.UserReader
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.IsolationMode
+import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
+import io.mockk.mockk
 import org.springframework.http.MediaType
 import org.springframework.mock.web.MockHttpServletRequest
 import org.springframework.mock.web.MockHttpServletResponse
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.authority.AuthorityUtils
 import org.springframework.security.web.authentication.AuthenticationFailureHandler
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 
-class RequestBodyUsernamePasswordAuthenticationFilterTest(
-    private val authenticationManager: AuthenticationManager,
-    private val authenticationFailureHandler: AuthenticationFailureHandler,
-    private val objectMapper: ObjectMapper,
-    private val tokenService: TokenService,
-    private val userReader: UserReader,
-) : SecurityTest() {
-    private lateinit var requestBodyUsernamePasswordAuthenticationFilter: RequestBodyUsernamePasswordAuthenticationFilter
-    private lateinit var request: MockHttpServletRequest
-    private lateinit var response: MockHttpServletResponse
+class RequestBodyUsernamePasswordAuthenticationFilterTest :
+    FunSpec(
+        {
+            isolationMode = IsolationMode.InstancePerLeaf
 
-    @BeforeEach
-    fun setUp() {
-        requestBodyUsernamePasswordAuthenticationFilter =
-            RequestBodyUsernamePasswordAuthenticationFilter(
-                authenticationManager,
-                objectMapper,
-                IssueJwtAuthenticationSuccessHandler(tokenService, objectMapper),
-                authenticationFailureHandler,
-            )
-        request = MockHttpServletRequest()
-        response = MockHttpServletResponse()
-    }
+            val authenticationManager = mockk<AuthenticationManager>()
+            val authenticationSuccessHandler = mockk<AuthenticationSuccessHandler>()
+            val authenticationFailureHandler = mockk<AuthenticationFailureHandler>()
+            val requestBodyUsernamePasswordAuthenticationFilter =
+                RequestBodyUsernamePasswordAuthenticationFilter(
+                    authenticationManager,
+                    jacksonObjectMapper(),
+                    authenticationSuccessHandler,
+                    authenticationFailureHandler,
+                )
 
-    @Test
-    fun `로그인 정보 추출 성공`() {
-        val username = "username123"
-        request.contentType = MediaType.APPLICATION_JSON_VALUE
-        request.setContent("{\"username\":\"$username\",\"password\":\"password123!\"}".toByteArray())
-        every { userReader.read(username) } returns UserBuilder(username = username).build()
+            context("로그인 정보 추출") {
+                val request = MockHttpServletRequest()
+                val response = MockHttpServletResponse()
 
-        val actual = requestBodyUsernamePasswordAuthenticationFilter.attemptAuthentication(request, response)
+                test("성공") {
+                    val username = "username123"
+                    val password = "password123!"
+                    request.contentType = MediaType.APPLICATION_JSON_VALUE
+                    request.setContent("{\"username\":\"$username\",\"password\":\"$password\"}".toByteArray())
+                    every { authenticationManager.authenticate(any()) } returns
+                        UsernamePasswordAuthenticationToken.authenticated(
+                            username,
+                            password,
+                            AuthorityUtils.NO_AUTHORITIES,
+                        )
 
-        actual.isAuthenticated shouldBe true
-    }
+                    val actual =
+                        requestBodyUsernamePasswordAuthenticationFilter.attemptAuthentication(request, response)
 
-    @Test
-    fun `정보가 없으면 추출에 실패한다`() {
-        request.contentType = MediaType.APPLICATION_JSON_VALUE
-        request.setContent("".toByteArray())
+                    actual.isAuthenticated shouldBe true
+                }
 
-        shouldThrow<BadCredentialsException> {
-            requestBodyUsernamePasswordAuthenticationFilter.attemptAuthentication(
-                request,
-                response,
-            )
-        }
-    }
-}
+                test("정보가 없으면 추출에 실패한다.") {
+                    request.contentType = MediaType.APPLICATION_JSON_VALUE
+                    request.setContent("".toByteArray())
+
+                    shouldThrow<BadCredentialsException> {
+                        requestBodyUsernamePasswordAuthenticationFilter.attemptAuthentication(request, response)
+                    }
+                }
+            }
+        },
+    )
