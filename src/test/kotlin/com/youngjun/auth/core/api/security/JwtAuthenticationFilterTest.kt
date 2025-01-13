78cfefbf3fdaf59b96e6e538d26221c994c47d69
YoungJun-L <dudwns328@naver.com>
2025-01-13 09:26:40 +0900

    JwtAuthenticationFilter 테스트 mocking 방식으로 수정

--- src/test/kotlin/com/youngjun/auth/core/api/security/JwtAuthenticationFilterTest.kt
+++ src/test/kotlin/com/youngjun/auth/core/api/security/JwtAuthenticationFilterTest.kt
@@ -1,16 +1,13 @@
 package com.youngjun.auth.core.api.security
 
-import com.youngjun.auth.core.api.support.SecurityTest
-import com.youngjun.auth.core.domain.token.TokenParser
 import com.youngjun.auth.core.domain.user.UserBuilder
-import com.youngjun.auth.core.domain.user.UserReader
+import io.kotest.core.spec.IsolationMode
+import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import io.mockk.Runs
 import io.mockk.every
-import jakarta.servlet.FilterChain
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpStatus
+import io.mockk.just
+import io.mockk.mockk
 import org.springframework.mock.web.MockFilterChain
 import org.springframework.mock.web.MockHttpServletRequest
 import org.springframework.mock.web.MockHttpServletResponse
@@ -19,61 +16,50 @@ import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.authentication.AuthenticationFailureHandler
 
-class JwtAuthenticationFilterTest(
-    private val authenticationManager: AuthenticationManager,
-    private val authenticationFailureHandler: AuthenticationFailureHandler,
-    private val tokenParser: TokenParser,
-    private val userReader: UserReader,
-) : SecurityTest() {
-    private lateinit var jwtAuthenticationFilter: JwtAuthenticationFilter
-    private lateinit var request: MockHttpServletRequest
-    private lateinit var response: MockHttpServletResponse
-    private lateinit var filterChain: FilterChain
+class JwtAuthenticationFilterTest :
+    FunSpec(
+        {
+            isolationMode = IsolationMode.InstancePerLeaf
 
-    @BeforeEach
-    fun setUp() {
-        jwtAuthenticationFilter =
-            JwtAuthenticationFilter(
-                BearerTokenResolver(),
-                authenticationManager,
-                authenticationFailureHandler,
-            )
-        request = MockHttpServletRequest()
-        response = MockHttpServletResponse()
-        filterChain = MockFilterChain()
-    }
+            val bearerTokenResolver = mockk<BearerTokenResolver>()
+            val authenticationManager = mockk<AuthenticationManager>()
+            val authenticationFailureHandler = mockk<AuthenticationFailureHandler>()
+            val jwtAuthenticationFilter =
+                JwtAuthenticationFilter(bearerTokenResolver, authenticationManager, authenticationFailureHandler)
 
-    @Test
-    fun `JWT 인증 처리 성공`() {
-        val value = "a.b.c"
-        val username = "username123"
-        request.addHeader(HttpHeaders.AUTHORIZATION, "Bearer $value")
-        every { tokenParser.parseSubject(value) } returns username
-        every { userReader.read(username) } returns UserBuilder(username = username).build()
+            afterTest { SecurityContextHolder.clearContext() }
 
-        jwtAuthenticationFilter.doFilter(request, response, filterChain)
+            context("JWT 인증 처리") {
+                val request = MockHttpServletRequest()
+                val response = MockHttpServletResponse()
+                val filterChain = MockFilterChain()
 
-        SecurityContextHolder.getContext().authentication.isAuthenticated shouldBe true
-    }
+                test("성공") {
+                    every { bearerTokenResolver.resolve(any()) } returns "token"
+                    every { authenticationManager.authenticate(any()) } returns
+                        JwtAuthenticationToken.authenticated(UserBuilder().build())
 
-    @Test
-    fun `헤더에 값이 없을 때 처리되지 않는다`() {
-        request.addHeader(HttpHeaders.AUTHORIZATION, "")
+                    jwtAuthenticationFilter.doFilter(request, response, filterChain)
 
-        jwtAuthenticationFilter.doFilter(request, response, filterChain)
+                    SecurityContextHolder.getContext().authentication.isAuthenticated shouldBe true
+                }
 
-        SecurityContextHolder.getContext().authentication shouldBe null
-    }
+                test("resolve 값이 없는 경우") {
+                    every { bearerTokenResolver.resolve(request) } returns null
 
-    @Test
-    fun `인증에 실패하면 실패 핸들러가 실행 된다`() {
-        val value = "a.b.c"
-        request.addHeader(HttpHeaders.AUTHORIZATION, "Bearer $value")
-        every { tokenParser.parseSubject(value) } throws BadCredentialsException("")
+                    jwtAuthenticationFilter.doFilter(request, response, filterChain)
 
-        jwtAuthenticationFilter.doFilter(request, response, filterChain)
+                    SecurityContextHolder.getContext().authentication shouldBe null
+                }
 
-        SecurityContextHolder.getContext().authentication shouldBe null
-        response.status shouldBe HttpStatus.UNAUTHORIZED.value()
-    }
-}
+                test("인증에 실패하는 경우") {
+                    every { bearerTokenResolver.resolve(any()) } throws BadCredentialsException("")
+                    every { authenticationFailureHandler.onAuthenticationFailure(any(), any(), any()) } just Runs
+
+                    jwtAuthenticationFilter.doFilter(request, response, filterChain)
+
+                    SecurityContextHolder.getContext().authentication shouldBe null
+                }
+            }
+        },
+    )
