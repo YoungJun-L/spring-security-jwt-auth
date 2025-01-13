4957d958def7eef1aac523a697b2d56cb5682379
YoungJun-L <dudwns328@naver.com>
2025-01-13 09:37:22 +0900

    UserDetailsExchangeFilter 테스트 컨테이너 없는 방식으로 변경

--- src/test/kotlin/com/youngjun/auth/core/api/security/UserDetailsExchangeFilterTest.kt
+++ src/test/kotlin/com/youngjun/auth/core/api/security/UserDetailsExchangeFilterTest.kt
@@ -1,48 +1,49 @@
 package com.youngjun.auth.core.api.security
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.youngjun.auth.core.api.support.SecurityTest
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.youngjun.auth.core.domain.user.UserBuilder
+import io.kotest.core.spec.IsolationMode
+import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
-import io.mockk.every
-import io.mockk.mockk
 import jakarta.servlet.http.HttpServletRequest
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
 import org.springframework.mock.web.MockFilterChain
 import org.springframework.mock.web.MockHttpServletRequest
 import org.springframework.mock.web.MockHttpServletResponse
-import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 
-class UserDetailsExchangeFilterTest(
-    private val objectMapper: ObjectMapper,
-) : SecurityTest() {
-    private lateinit var userDetailsExchangeFilter: UserDetailsExchangeFilter
-    private lateinit var request: MockHttpServletRequest
-    private lateinit var response: MockHttpServletResponse
-    private lateinit var filterChain: MockFilterChain
+class UserDetailsExchangeFilterTest :
+    FunSpec(
+        {
+            isolationMode = IsolationMode.InstancePerLeaf
 
-    @BeforeEach
-    fun setUp() {
-        userDetailsExchangeFilter = UserDetailsExchangeFilter(objectMapper)
-        request = MockHttpServletRequest()
-        response = MockHttpServletResponse()
-        filterChain = MockFilterChain()
-    }
+            val userDetailsExchangeFilter = UserDetailsExchangeFilter(jacksonObjectMapper())
 
-    @Test
-    fun `인증 정보를 쿠키로 교환 성공`() {
-        val username = "username123"
-        val authentication = mockk<Authentication>()
-        SecurityContextHolder.getContext().authentication = authentication
-        every { authentication.principal } returns username
-        every { authentication.details } returns emptyMap<String, Any>()
+            afterTest { SecurityContextHolder.clearContext() }
 
-        userDetailsExchangeFilter.doFilter(request, response, filterChain)
+            context("인증 정보를 쿠키로 교환") {
+                val request = MockHttpServletRequest()
+                val response = MockHttpServletResponse()
+                val filterChain = MockFilterChain()
 
-        val request = filterChain.request as HttpServletRequest
-        val actual = request.cookies.first { it.name == "user" }.value
-        actual shouldBe "{\"username\":\"$username\",\"details\":{}}"
-        SecurityContextHolder.getContext().authentication shouldBe null
-    }
-}
+                test("성공") {
+                    val username = "username123"
+                    SecurityContextHolder.getContext().authentication =
+                        JwtAuthenticationToken.authenticated(UserBuilder(username = username).build())
+
+                    userDetailsExchangeFilter.doFilter(request, response, filterChain)
+
+                    val actual = (filterChain.request as HttpServletRequest).cookies.first { it.name == "user" }.value
+                    actual shouldBe "{\"username\":\"$username\",\"details\":{}}"
+                }
+
+                test("성공하면 인증 정보는 비워진다.") {
+                    SecurityContextHolder.getContext().authentication =
+                        JwtAuthenticationToken.authenticated(UserBuilder().build())
+
+                    userDetailsExchangeFilter.doFilter(request, response, filterChain)
+
+                    SecurityContextHolder.getContext().authentication shouldBe null
+                }
+            }
+        },
+    )
