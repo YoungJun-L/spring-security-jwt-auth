82a2d4fb24eb80cb9b25ad1c4def685d00583e48
YoungJun-L <dudwns328@naver.com>
2025-01-23 23:39:42 +0900

    ControllerAdvice 수정

--- core/src/main/kotlin/com/youngjun/core/api/controller/ApiControllerAdvice.kt
+++ core/src/main/kotlin/com/youngjun/core/api/controller/ApiControllerAdvice.kt
@@ -1,12 +1,10 @@
 package com.youngjun.core.api.controller
 
 import com.youngjun.core.support.error.CoreException
-import com.youngjun.core.support.error.ErrorType.DEFAULT_ERROR
 import com.youngjun.core.support.response.ApiResponse
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.logging.LogLevel
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -25,13 +23,5 @@ class ApiControllerAdvice {
         return ResponseEntity
             .status(ex.errorType.status)
             .body(ApiResponse.error(ex.errorType, ex.data))
-    }
-
-    @ExceptionHandler(Exception::class)
-    fun handleException(ex: Exception): ResponseEntity<ApiResponse<Any>> {
-        log.info("Exception: {}", ex.message, ex)
-        return ResponseEntity
-            .status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .body(ApiResponse.error(DEFAULT_ERROR))
     }
 }
