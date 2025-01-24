f25534b2b0d727d6187fd7fea8b337ee57af9a5d
YoungJun-L <dudwns328@naver.com>
2025-01-24 13:39:47 +0900

    TimeExtensions 추가

--- auth/src/main/kotlin/com/youngjun/auth/core/domain/support/TimeExtensions.kt
+++ auth/src/main/kotlin/com/youngjun/auth/core/domain/support/TimeExtensions.kt
@@ -4,9 +4,16 @@ import java.time.Duration
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
+import java.time.ZoneOffset
 
 fun LocalDateTime.toEpochSecond(): Long = this.atZone(ZoneId.systemDefault()).toEpochSecond()
 
 fun LocalDateTime.toInstant(): Instant = this.atZone(ZoneId.systemDefault()).toInstant()
 
+val EPOCH: LocalDateTime = LocalDateTime.ofInstant(Instant.EPOCH, ZoneOffset.UTC)
+
+inline val Int.seconds: Duration get() = Duration.ofSeconds(this.toLong())
+
 inline val Int.hours: Duration get() = Duration.ofHours(this.toLong())
+
+inline val Int.days: Duration get() = Duration.ofDays(this.toLong())
