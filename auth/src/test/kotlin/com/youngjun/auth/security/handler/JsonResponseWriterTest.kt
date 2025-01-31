6651b7cd17584c5ff9515b8cf7dc987815c9f93b
YoungJun-L <dudwns328@naver.com>
2025-01-31 18:27:53 +0900

    JsonResponseWriter 테스트 추가

---
+++ auth/src/test/kotlin/com/youngjun/auth/security/handler/JsonResponseWriterTest.kt
@@ -0,0 +1,34 @@
+package com.youngjun.auth.security.handler
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.youngjun.auth.support.SecurityTest
+import com.youngjun.auth.support.response.AuthResponse
+import com.youngjun.auth.support.response.ResultType
+import io.kotest.core.spec.IsolationMode
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import org.springframework.http.HttpStatus
+import org.springframework.mock.web.MockHttpServletResponse
+
+@SecurityTest
+class JsonResponseWriterTest :
+    FunSpec(
+        {
+            isolationMode = IsolationMode.InstancePerLeaf
+
+            val objectMapper = jacksonObjectMapper()
+            val jsonResponseWriter = JsonResponseWriter(objectMapper)
+
+            context("로그인") {
+                val response = MockHttpServletResponse()
+
+                test("성공") {
+                    jsonResponseWriter.write(response, HttpStatus.OK, AuthResponse.success())
+
+                    response.status shouldBe HttpStatus.OK.value()
+                    objectMapper.readValue<AuthResponse<Any>>(response.contentAsByteArray).status shouldBe ResultType.SUCCESS
+                }
+            }
+        },
+    )
